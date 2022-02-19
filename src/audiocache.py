c457452307e65d418c44afa90ce28f419a5014b9
Abdo <abd.nh25@gmail.com>
2022-02-20 00:00:55 +0300

    Cache failed audio lookups

--- src/audiocache.py
+++ src/audiocache.py
@@ -1,9 +1,10 @@
 import os
 from typing import List
 import re
+import json
 
 from anki.utils import stripHTML
-from tdk import TDK
+import tdk
 
 from .consts import *
 
@@ -26,13 +27,31 @@ def get_cached_audio(disk_word: str) -> List[str]:
     return files
 
 
+def cache_failed_lookup(disk_word: str) -> None:
+    with open(FAILED_LOOKUP_CACHE_FILE, "r") as f:
+        lookups: List = json.load(f)
+    lookups.append(disk_word)
+    with open(FAILED_LOOKUP_CACHE_FILE, "w") as f:
+        json.dump(lookups, f)
+
+
+def has_cached_failed_lookup(disk_word: str) -> bool:
+    with open(FAILED_LOOKUP_CACHE_FILE, "r") as f:
+        lookups = set(json.load(f))
+        return disk_word in lookups
+
+
 def get_audio(word: str) -> List[str]:
     word = normalize_word(word)
     disk_word = to_disk_name(word)
     files = get_cached_audio(disk_word)
-    if len(files) == 0:
-        tdk = TDK(word)
-        links = tdk.audio_links
-        tdk.word = disk_word
-        files = tdk.download_audio(AUDIO_CACHE_DIR)
+    if len(files) == 0 and not has_cached_failed_lookup(disk_word):
+        query = tdk.TDK(word)
+        try:
+            links = query.audio_links
+            query.word = disk_word
+            files = query.download_audio(AUDIO_CACHE_DIR)
+        except tdk.NoAudioError:
+            cache_failed_lookup(disk_word)
+            raise
     return files
--- src/consts.py
+++ src/consts.py
@@ -5,4 +5,8 @@ ADDON_NAME_LONG = "TRDict - Turkish Dictionary"
 ADDON_DIR = os.path.dirname(__file__)
 USER_FILES = os.path.join(ADDON_DIR, "user_files")
 AUDIO_CACHE_DIR = os.path.join(USER_FILES, "audiocache")
+FAILED_LOOKUP_CACHE_FILE = os.path.join(USER_FILES, "failed_lookups.json")
 os.makedirs(AUDIO_CACHE_DIR, exist_ok=True)
+if not os.path.exists(FAILED_LOOKUP_CACHE_FILE):
+    with open(FAILED_LOOKUP_CACHE_FILE, "w") as f:
+        f.write("[]\n")
