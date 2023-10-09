4ebbb17039914596a7a27733db08697240529762
Abdo <abdo@abdnh.net>
2023-10-09 17:17:36 +0300

    Fix error on non-existent 'lookups' dir

--- src/consts.py
+++ src/consts.py
@@ -1,13 +1,12 @@
-import os
-
 from ankiutils.consts import get_consts
 
 consts = get_consts(__name__)
-USER_FILES = os.path.join(consts.dir, "user_files")
-AUDIO_CACHE_DIR = os.path.join(USER_FILES, "audiocache")
-FAILED_LOOKUP_CACHE_FILE = os.path.join(USER_FILES, "lookups", "failed_lookups.json")
-os.makedirs(AUDIO_CACHE_DIR, exist_ok=True)
-if not os.path.exists(FAILED_LOOKUP_CACHE_FILE):
+USER_FILES = consts.dir / "user_files"
+AUDIO_CACHE_DIR = USER_FILES / "audiocache"
+FAILED_LOOKUP_CACHE_FILE = USER_FILES / "lookups" / "failed_lookups.json"
+AUDIO_CACHE_DIR.mkdir(exist_ok=True, parents=True)
+FAILED_LOOKUP_CACHE_FILE.parent.mkdir(exist_ok=True, parents=True)
+if not FAILED_LOOKUP_CACHE_FILE.exists():
     with open(FAILED_LOOKUP_CACHE_FILE, "w", encoding="utf-8") as f:
         f.write("[]\n")
 
